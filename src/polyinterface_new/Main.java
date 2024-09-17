a2a9dc9c3ddd68a8f0bca704755a71e1a1441bd6
farisabqori <farisabq99@gmail.com>
2024-09-17 17:12:32 +0700

    feat: nambah kite, main, Parallelogram

--- src/polyinterface_new/Main.java
+++ src/polyinterface_new/Main.java
@@ -8,12 +8,23 @@ public class Main{
 //        System.out.println(Segitiga.getPerimeter());
 //        Segitiga.drawIllustration();
 
-        Geom2D Trapesium = new Trapezoid(16, 10, 4, 5);
+//        Geom2D Trapesium = new Trapezoid(16, 10, 4, 5);
+//
+//        System.out.println(Trapesium.getArea());
+//        System.out.println(Trapesium.getPerimeter());
+//        Trapesium.drawIllustration();
 
-        System.out.println(Trapesium.getArea());
-        System.out.println(Trapesium.getPerimeter());
-        Trapesium.drawIllustration();
+//        Geom2D Jajargenjang = new Parallelogram(4,5, 10);
+//
+//        System.out.println(Jajargenjang.getArea());
+//        System.out.println(Jajargenjang.getPerimeter());
+//        Jajargenjang.drawIllustration();
 
+          Geom2D Layangan = new Kite(10, 5, 6,14);
+
+          System.out.println(Layangan.getArea());
+          System.out.println(Layangan.getPerimeter());
+          Layangan.drawIllustration();
 
     }
 }
